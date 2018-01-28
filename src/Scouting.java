a9990f3f0014e66179d7e7c3fface72f04918560
Tony Pan <tonypan2000@gmail.com>
2018-01-28 14:03:19 -0500

    update the mode selection

--- src/Scouting.java
+++ src/Scouting.java
@@ -3,20 +3,22 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import acm.graphics.GCanvas;
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
+import acm.program.GraphicsProgramInterface;
 
 public class Scouting extends GraphicsProgram {
 
-	@SuppressWarnings("unused")
-	private boolean gameOn; // True after clicking the start button
-	@SuppressWarnings("unused")
-	private boolean isAuton; // First 15 seconds of the match
+	private boolean gameOn; // true after clicking the start button
+	private boolean isAuton; // first 15 seconds of the match
 
-	// All the interactors that will be called more than once
+	// all the interactors that will be called more than once
 	private GCanvas canvas = new GCanvas();
 	private JTextField matchNum = new JTextField(5);
 	private JTextField red1;
@@ -25,15 +27,7 @@ public class Scouting extends GraphicsProgram {
 	private JTextField blue1;
 	private JTextField blue2;
 	private JTextField blue3;
-	private JLabel Red1 = new JLabel("RED 1");
-	private JLabel Red2 = new JLabel("RED 2");
-	private JLabel Red3 = new JLabel("RED 3");
-	private JLabel Blue1 = new JLabel("BLUE 1");
-	private JLabel Blue2 = new JLabel("BLUE 2");
-	private JLabel Blue3 = new JLabel("BLUE 3");
-	private JLabel pending;
-	private JLabel auton;
-	private JLabel teleop;
+	private JComboBox mode;
 	private JButton start;
 	private JButton reset;
 	private JButton submit;
@@ -42,11 +36,22 @@ public class Scouting extends GraphicsProgram {
 		initiation();
 	}
 
-	
-	/**
-	 * Adds all graphics to the canvas in the beginning.
-	 **/
+	/*
+	 * Adds all the graphics to canvas in the beginning
+	 */
 	private void initiation() {
+		JLabel Red1 = new JLabel("RED 1");
+		Red1.setForeground(Color.RED);
+		JLabel Red2 = new JLabel("RED 2");
+		Red2.setForeground(Color.RED);
+		JLabel Red3 = new JLabel("RED 3");
+		Red3.setForeground(Color.RED);
+		JLabel Blue1 = new JLabel("BLUE 1");
+		Blue1.setForeground(Color.BLUE);
+		JLabel Blue2 = new JLabel("BLUE 2");
+		Blue2.setForeground(Color.BLUE);
+		JLabel Blue3 = new JLabel("BLUE 3");
+		Blue3.setForeground(Color.BLUE);
 		setCanvasSize(1000, 457);
 		canvas.setSize(1000, 457);
 		add(canvas, 0, 0);
@@ -55,6 +60,9 @@ public class Scouting extends GraphicsProgram {
 		canvas.add(field, 0, 30);
 		JLabel match = new JLabel("Match");
 		match.setFont(new Font("Sans Serif", Font.BOLD | Font.ITALIC, 20));
+		JLabel selectMode = new JLabel("Mode");
+		selectMode.setFont(new Font("Sans Serif", Font.BOLD, 20));
+		canvas.add(selectMode, getWidth() / 2 - 100, 5);
 		canvas.add(match, 30, 5);
 		canvas.add(Red1, 20, 130);
 		canvas.add(Red2, 20, 225);
@@ -65,13 +73,10 @@ public class Scouting extends GraphicsProgram {
 		addInteractors();
 	}
 
-	/**
-	 * Adds the interactors to the screen.
-	 **/
+	/*
+	 * adds the interactors to the screen
+	 */
 	private void addInteractors() {
-		pending = new JLabel("Pending");
-		auton = new JLabel("Autonomous");
-		teleop = new JLabel("Teleop");
 		start = new JButton("Start");
 		reset = new JButton("Reset");
 		submit = new JButton("Submit");
@@ -81,14 +86,11 @@ public class Scouting extends GraphicsProgram {
 		blue1 = new JTextField(10);
 		blue2 = new JTextField(10);
 		blue3 = new JTextField(10);
-		pending.setFont(new Font("Sans Serif", Font.BOLD, 20));
-		pending.setForeground(Color.RED);
-		auton.setFont(new Font("Sans Serif", Font.BOLD, 20));
-		auton.setForeground(Color.ORANGE);
-		teleop.setFont(new Font("Sans Serif", Font.BOLD, 20));
-		teleop.setForeground(Color.GREEN);
-		canvas.add(pending, getWidth() / 2 - 100, 5);
+		String[] modes = { "Pending", "Autonomous", "Teleop" };
+		mode = new JComboBox(modes);
+		mode.setSelectedIndex(0);
 		canvas.add(matchNum, 100, 10);
+		canvas.add(mode, getWidth() / 2, 10);
 		canvas.add(red1, 20, 145);
 		canvas.add(red2, 20, 240);
 		canvas.add(red3, 20, 335);
@@ -98,30 +100,44 @@ public class Scouting extends GraphicsProgram {
 		canvas.add(start, getWidth() - 300, 10);
 		canvas.add(reset, getWidth() - 200, 10);
 		canvas.add(submit, getWidth() - 100, 10);
+
 		addActionListeners();
 	}
 
-	
-	/**
-	 * Listens for and responds to action commands.
-	 **/
+	/*
+	 * listens for and responds to action commands
+	 */
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == start) {
-			// When the match starts
+			// when the match starts
 			gameOn = true;
 			isAuton = true;
-			
-		} else if (event.getSource() == reset) {
-			// When the match resets
+			mode.setSelectedIndex(1);
+		}
+		if (event.getSource() == reset) {
+			// when the match resets
+			gameOn = false;
+			isAuton = true;
+			mode.setSelectedIndex(0);
+		}
+		if (event.getSource() == submit) {
+			gameOn = false;
+			isAuton = true;
+			mode.setSelectedIndex(0);
+		}
+		if (event.getSource() == mode) {
+			if (mode.getSelectedIndex() == 3) {
+				// teleop mode
+				isAuton = false;
+			}
 		}
 	}
 
-	
-	/**
-	 * Keep statistics for one match.
-	 **/
+	/*
+	 * Keep stats for one match
+	 */
 	private void recordOneGame() {
-		
+
 	}
 
 }
